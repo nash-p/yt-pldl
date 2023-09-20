8e2da51436ce96de167ee115125c1daabbd0a36d
Nash P <nashakpd@gmail.com>
2023-09-20 19:59:10 +0100

    Better formatting

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import googleapiclient.discovery
 from urllib.parse import parse_qs, urlparse
-from secrets import API_KEY
+from secret import API_KEY
 
-#extract playlist id from url
-url = 'https://www.youtube.com/playlist?list=PL8uLmtrEOEHWuyK0jfrfZ4t4QhW59JFf7'
+# extract playlist id from url
+url = "https://www.youtube.com/playlist?list=PL8uLmtrEOEHWuyK0jfrfZ4t4QhW59JFf7"
 query = parse_qs(urlparse(url).query, keep_blank_values=True)
 playlist_id = query["list"][0]
 
-print(f'get all playlist items links from {playlist_id}')
-youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = API_KEY)
+print(f"get all playlist items links from {playlist_id}")
+youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=API_KEY)
 
 request = youtube.playlistItems().list(
-    part = "snippet",
-    playlistId = playlist_id,
-    maxResults = 50
+    part="snippet", playlistId=playlist_id, maxResults=50
 )
 response = request.execute()
 
@@ -24,7 +22,8 @@ while request is not None:
     request = youtube.playlistItems().list_next(request, response)
 
 print(f"total: {len(playlist_items)}")
-print([ 
+playlist_url_List = [
     f'https://www.youtube.com/watch?v={t["snippet"]["resourceId"]["videoId"]}'
     for t in playlist_items
-])
+]
+print(playlist_url_List)
